29ae9ad189561097eda46dbe834693c621c8c865
Grigory Vasilev <grishavasilev62@gmail.com>
2022-03-19 21:10:25 +0300

    Поправки в запросе

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,10 +10,16 @@ namespace AVLabWeb.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Создаем интерфейс для получения данных из бд. Все остальное скрыто.
+        /// </summary>
         private IDbProvider DbProvider = new DbProvider
             (@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Мвидео\Desktop\AVLabWeb\Students.mdb");
 
-        // GET: Home
+        /// <summary>
+        /// Главная страница. Выводит список студентов.
+        /// </summary>
+        /// <returns> Страница со списком студентов. </returns>
         [HttpGet]
         public ActionResult Index()
         {

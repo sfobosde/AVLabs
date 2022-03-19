2a17371661e625526ee9711a81825cb905421f1a
Grigory Vasilev <grishavasilev62@gmail.com>
2022-03-19 19:11:50 +0300

    Добавленая архитектура работы с БД

---
+++ DbWork/DbProvider.cs
@@ -0,0 +1,32 @@
+﻿using AVLabWeb.DbWork.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AVLabWeb.DbWork
+{
+	/// <summary>
+	/// Объект, возвращающий выборки по запросам к базе данных через свои методы.
+	/// </summary>
+	public class DbProvider : DbProviderFactory
+	{
+		/// <summary>
+		/// Получить список студентов.
+		/// </summary>
+		/// <returns> Список студентов. </returns>
+		public override List<Student> GetStudentsList()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Инициализация подключения.
+		/// </summary>
+		/// <param name="connectionString"> Строка подключения. </param>
+		public DbProvider(string connectionString) : base(connectionString)
+		{
+
+		}
+	}
+}
---
+++ DbWork/DbProviderFactory.cs
@@ -0,0 +1,43 @@
+﻿using AVLabWeb.DbWork.Entities;
+using AVLabWeb.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.OleDb;
+
+namespace AVLabWeb.DbWork
+{
+	/// <summary>
+	/// Класс - "Фабрика" для работы с БД.
+	/// </summary>
+	public abstract class DbProviderFactory : IDbProvider
+	{
+		/// <summary>
+		/// Строка подключения к БД.
+		/// </summary>
+		protected string connectionString;
+
+		/// <summary>
+		/// Подключение к БД.
+		/// </summary>
+		protected OleDbConnection dbConnection;
+
+		/// <summary>
+		/// Не реализовываем. Реализуем в прототипе.
+		/// </summary>
+		/// <returns> Список студентов. </returns>
+		public abstract List<Student> GetStudentsList();
+
+		/// <summary>
+		/// Подключение к БД с использованием connectionString.
+		/// </summary>
+		/// <param name="connectionString"> Данные для подключения. </param>
+		public DbProviderFactory(string connectionString)
+		{
+			this.connectionString = connectionString;
+
+			dbConnection = new OleDbConnection(connectionString);
+		}
+	}
+}
